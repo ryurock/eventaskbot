801104b58876795240f522166c5ca536cffa8832
vagrant <ryusukekimura3@gmail.com>
2013-12-26 01:47:41 +0900

    API collector Base specのリファクタリング

--- spec/eventaskbot/api/collector_spec.rb
+++ spec/eventaskbot/api/collector_spec.rb
@@ -8,9 +8,10 @@ require 'eventaskbot/api/collector'
 describe Eventaskbot::Api::Collector, "Eventaskbot Collector API Module" do
   before(:each) do
     Eventaskbot::Api::Collector.reset
+    @assert = {:service => [:yammer, :redmine]}
   end
 
-  it "クラスである事の確認" do
+  it "モジュールである事の確認" do
     expect(Eventaskbot::Api::Collector.class).to eq(Module)
   end
 
@@ -23,13 +24,11 @@ describe Eventaskbot::Api::Collector, "Eventaskbot Collector API Module" do
   end
 
   it "in_groupの設定を与えた値が取得できること" do
-    assert = {:in_group => {:service => [:yammer, :redmine]}}
-
     Eventaskbot::Api::Collector.configure do |c|
-      c.in_group = {:service => [:yammer, :redmine]}
+      c.in_group = @assert
     end
 
-    expect(Eventaskbot::Api::Collector.options).to eq(assert)
+    expect(Eventaskbot::Api::Collector.options[:in_group]).to eq(@assert)
   end
 
   it "groupの設定を与えても例外にならないこと" do
@@ -41,49 +40,43 @@ describe Eventaskbot::Api::Collector, "Eventaskbot Collector API Module" do
   end
 
   it "groupの設定を与えた値が取得できること" do
-    assert = {:service => [:yammer, :redmine]}
-
     Eventaskbot::Api::Collector.configure do |c|
-      c.group = assert
+      c.group = @assert
     end
 
-    expect(Eventaskbot::Api::Collector.options[:group]).to eq(assert)
+    expect(Eventaskbot::Api::Collector.options[:group]).to eq(@assert)
   end
 
   it "usersの設定を与えても例外にならないこと" do
     expect{
       Eventaskbot::Api::Collector.configure do |c|
-        c.users = {:service => [:yammer, :redmine]}
+        c.users = @assert
       end
     }.to_not raise_error
   end
 
   it "usersの設定を与えた値が取得できること" do
-    assert = {:service => [:yammer, :redmine]}
-
     Eventaskbot::Api::Collector.configure do |c|
-      c.users = assert
+      c.users = @assert
     end
 
-    expect(Eventaskbot::Api::Collector.options[:users]).to eq(assert)
+    expect(Eventaskbot::Api::Collector.options[:users]).to eq(@assert)
   end
 
   it "taskの設定を与えても例外にならないこと" do
     expect{
       Eventaskbot::Api::Collector.configure do |c|
-        c.task = {:service => [:yammer, :redmine]}
+        c.task = @assert
       end
     }.to_not raise_error
   end
 
   it "usersの設定を与えた値が取得できること" do
-    assert = {:service => [:yammer, :redmine]}
-
     Eventaskbot::Api::Collector.configure do |c|
-      c.task = assert
+      c.task = @assert
     end
 
-    expect(Eventaskbot::Api::Collector.options[:task]).to eq(assert)
+    expect(Eventaskbot::Api::Collector.options[:task]).to eq(@assert)
   end
 
 end
