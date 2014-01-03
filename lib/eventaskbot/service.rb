6132eca88aaf828f2f7a9a9de166fabd72b31627
vagrant <ryusukekimura3@gmail.com>
2014-01-04 04:02:31 +0900

    serviceレイヤーの親クラスを作成

---
+++ lib/eventaskbot/service.rb
@@ -0,0 +1,87 @@
+#
+# サービスに関するベースクラス
+#
+module Eventaskbot
+  class Service
+    attr_reader :params, :services
+
+    def initialize
+      @params   = {}
+      @services = []
+    end
+
+    def config_merge
+      opts = Eventaskbot.options
+      @params   = deep_merge @params, opts[:service] if opts.key?(:service)
+
+      sub_config_name = "Eventaskbot::Api::#{opts[:api][:type].to_s.capitalize}.options"
+      sub_config      = eval sub_config_name
+      api_name        = opts[:api][:name].gsub(/-/, "_").to_sym
+
+      @params = deep_merge(@params, sub_config[api_name][:service]) if sub_config.nil? == false && sub_config.key?(api_name) && sub_config[api_name].nil? == false && sub_config[api_name].key?(:service)
+      @params = deep_merge @params, opts[:api][:params][:service] if opts[:api][:params].key?(:service)
+
+      #sub設定でサービスの指定がある場合は削除する
+      if sub_config.nil? == false && sub_config.key?(api_name) && sub_config[api_name].nil? == false && sub_config[api_name].key?(:services)
+        @params = @params.inject({}) do |h,(k,v)|
+          h[k] = v unless sub_config[api_name][:services].index(k).nil?
+          h
+        end
+      end
+
+      #apiのパラメーターでサービスの指定がある場合は削除する
+      if opts[:api][:params].key?(:services)
+        @params = @params.inject({}) do |h,(k,v)|
+          h[k] = v unless opts[:api][:params][:services].index(k).nil?
+          h
+        end
+      end
+
+      @services = @params.keys
+
+      return self unless has_service?
+
+      @services.each do |v|
+        path = File.expand_path("../services/#{v}/#{opts[:api][:name].gsub(/-/,"_")}",__FILE__)
+        require path
+
+        klass_name = opts[:api][:name].split("-").inject([]){ |a,v| a.push(v.capitalize) }
+        klass = "Eventaskbot::Services::#{v.capitalize}::#{klass_name.join("")}.new"
+        @params[:klass] = eval klass
+      end
+
+      self
+    end
+
+    #
+    # serviceが存在するか？
+    # @return [Boolean] true 存在する | false 存在しない
+    #
+    def has_service?
+      return true if @services.size > 0
+      false
+    end
+
+    private
+
+      #
+      # 階層の深いマージ
+      # @param service[Hash] マージ元
+      # @param opts[Hash] マージしたい値
+      # @return [Hash] マージした値
+      #
+      def deep_merge(service, opts)
+        if service.size > 0
+          service = service.inject({}) do |h,(k,v)|
+            h[k] = v
+            h[k] = h[k].merge(opts[k]) if opts.key? k
+            h
+          end
+        else
+          service = opts
+        end
+
+        service
+      end
+  end
+end
---
+++ spec/eventaskbot/service_spec.rb
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+
+require File.expand_path(File.join('../', 'spec_helper'), File.dirname(__FILE__))
+
+require 'rspec/mocks/standalone'
+
+require 'eventaskbot'
+require 'eventaskbot/service'
+
+describe Eventaskbot::Service, "Eventaskbot Service Class" do
+  before(:each) do
+    Eventaskbot.reset
+    Eventaskbot.configure do |c|
+      c.service = { :yammer => { :client_id => "hoge", :client_secret => "fuga" } }
+      c.config_file = {:path => "EvetnaskbotFile"}
+      c.response = nil
+      c.plugin_dir = nil
+    end
+    Eventaskbot::Api::Auth.reset
+  end
+
+  it "クラスである事の確認" do
+    expect(Eventaskbot::Service.class).to eq(Class)
+  end
+
+  it "config_mergeメソッド実行時でベースの設定しか設定をしていない場合の戻り値はベースの設定とイコール" do
+    assert = { :yammer => { :client_id => "hoge", :client_secret => "fuga" } }
+
+    Eventaskbot.configure do |c|
+      c.api = { :name => 'get-oauth-token', :type => :auth, :params => {} }
+    end
+
+    service = Eventaskbot::Service.new
+    service.config_merge
+    service.params.delete(:klass)
+    expect(service.params).to eq(assert)
+  end
+
+  it "config_mergeメソッド実行時でベースの設定+sub設定をしている場合の戻り値はベースの設定とマージされる" do
+    assert = { :yammer => { :client_id => "hoge", :client_secret => "fuga", :user => "user1" } }
+
+    Eventaskbot.configure do |c|
+      c.api = { :name => 'get-oauth-token', :type => :auth, :params => {} }
+    end
+
+    Eventaskbot::Api::Auth.configure do |c|
+      c.get_oauth_token = {:service => { :yammer => { :user => "user1"} } }
+    end
+
+    service = Eventaskbot::Service.new
+    service.config_merge
+    service.params.delete(:klass)
+    expect(service.params).to eq(assert)
+  end
+
+  it "config_mergeメソッド実行時でベースの設定+sub設定+params設定をしている場合の戻り値はベースの設定とマージされる" do
+    assert = { :yammer => { :client_id => "hoge", :client_secret => "fuga", :user => "user1", :pass => "pass" } }
+
+    Eventaskbot.configure do |c|
+      c.api = { 
+        :name => 'get-oauth-token',
+        :type => :auth,
+        :params => { 
+          :service => {
+            :yammer => { :pass => "pass" }
+          }
+        }
+      }
+    end
+
+    Eventaskbot::Api::Auth.configure do |c|
+      c.get_oauth_token = {:service => { :yammer => { :user => "user1"} } }
+    end
+
+    service = Eventaskbot::Service.new
+    service.config_merge
+    service.params.delete(:klass)
+    expect(service.params).to eq(assert)
+  end
+
+  it "config_mergeメソッド実行時でベースの設定+sub設定をしているがsub設定でserviceの指定で:yammerを除外しているので中身は空のHash" do
+    assert = { }
+
+    Eventaskbot.configure do |c|
+      c.api = { :name => 'get-oauth-token', :type => :auth, :params => {} }
+    end
+
+    Eventaskbot::Api::Auth.configure do |c|
+      c.get_oauth_token = {
+        :service => { :yammer => { :user => "user1"} },
+        :services => [:redmine]
+      }
+    end
+
+    service = Eventaskbot::Service.new
+    service.config_merge
+    expect(service.params).to eq(assert)
+  end
+
+  it "config_mergeメソッド実行時でベースの設定+sub設定をしているがapiパラメーターの設定でserviceの指定で:yammerを除外しているので中身は空のHash" do
+    assert = { }
+
+    Eventaskbot.configure do |c|
+      c.api = { :name => 'get-oauth-token', :type => :auth, :params => {:services => [:redmine]} }
+    end
+
+    Eventaskbot::Api::Auth.configure do |c|
+      c.get_oauth_token = {
+        :service => { :yammer => { :user => "user1"} }
+      }
+    end
+
+    service = Eventaskbot::Service.new
+    service.config_merge
+    expect(service.params).to eq(assert)
+  end
+
+  it "config_mergeメソッド実行時でserviceがない場合の@servicesは空の配列" do
+    assert = []
+
+    Eventaskbot.configure do |c|
+      c.api = { :name => 'get-oauth-token', :type => :auth, :params => {:services => [:redmine]} }
+    end
+
+    Eventaskbot::Api::Auth.configure do |c|
+      c.get_oauth_token = {
+        :service => { :yammer => { :user => "user1"} }
+      }
+    end
+
+    service = Eventaskbot::Service.new
+    service.config_merge
+    expect(service.services).to eq(assert)
+  end
+
+  it "config_mergeメソッド実行時でserviceがある場合の@servicesは配列に値が入っている" do
+    assert = [:yammer]
+
+    Eventaskbot.configure do |c|
+      c.api = { :name => 'get-oauth-token', :type => :auth, :params => {} }
+    end
+
+    Eventaskbot::Api::Auth.configure do |c|
+      c.get_oauth_token = {
+        :service => { :yammer => { :user => "user1"} }
+      }
+    end
+
+    service = Eventaskbot::Service.new
+    service.config_merge
+    expect(service.services).to eq(assert)
+  end
+
+  it "has_service?メソッド実行時でserviceがある場合はtrue" do
+    Eventaskbot.configure do |c|
+      c.api = { :name => 'get-oauth-token', :type => :auth, :params => {} }
+    end
+
+    Eventaskbot::Api::Auth.configure do |c|
+      c.get_oauth_token = {
+        :service => { :yammer => { :user => "user1"} }
+      }
+    end
+
+    service = Eventaskbot::Service.new
+    service.config_merge
+    expect(service.has_service?).to eq(true)
+  end
+
+  it "has_service?メソッド実行時でserviceがない場合はfalse" do
+    Eventaskbot.configure do |c|
+      c.api = { :name => 'get-oauth-token', :type => :auth, :params => {} }
+    end
+
+    Eventaskbot::Api::Auth.configure do |c|
+      c.get_oauth_token = {
+        :service => { :yammer => { :user => "user1"} },
+        :services => [:redmine]
+      }
+    end
+
+    service = Eventaskbot::Service.new
+    service.config_merge
+    expect(service.has_service?).to eq(false)
+  end
+end
