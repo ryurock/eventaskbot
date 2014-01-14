510e074fa0eed2eb8f3d6e4629b30aa4c061660b
vagrant <ryusukekimura3@gmail.com>
2014-01-15 01:10:14 +0900

    サービスの部分を各サービスモジュールに委譲

--- lib/eventaskbot/api/auth/get_oauth_token.rb
+++ lib/eventaskbot/api/auth/get_oauth_token.rb
@@ -8,18 +8,22 @@ module Eventaskbot
         attr_accessor :res
         def execute(params)
           opts = Auth.option(:get_oauth_token)
+          pp opts
           @res = {:status => :fail, :message => ""}
 
           if opts.nil?
             @res[:message] = "[Failed] Setting service parametor not found"
             return @res
           end
-          
-#            unless v.key?(:klass)
-#              @res[:message] = "[Failed] service Class not found"
-#              return @res
-#            end
-#            @res = v[:klass].get_oauth_token(v)
+
+          opts.each do |service_name, v|
+            unless v.key?(:klass)
+              @res[:message] = "[Failed] service Class not found"
+              return @res
+            end
+
+            @res = v[:klass].execute(v)
+          end
 
           @res
         end
